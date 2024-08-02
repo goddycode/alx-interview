d1af268195be2576b56c801a4207befcd91fd4ff
goddycode <odhisgoddy24@gmail.com>
2024-08-02 01:53:15 +0000

    Updating '0-nqueens.py' file commit #1

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,54 +1,59 @@
 #!/usr/bin/python3
-"""
-N queens
-"""
-
+"""N Queens"""
 import sys
 
+
+def print_board(board, n):
+    """Print allocated positions to the queen"""
+    b = []
+
+    for i in range(n):
+        for j in range(n):
+            if j == board[i]:
+                b.append([i, j])
+    print(b)
+
+
+def is_position_safe(board, i, j, r):
+    """Checks if the position is safe for the queen"""
+    return board[i] in (j, j - i + r, i - r + j)
+
+
+def safe_positions(board, row, n):
+    """Find all safe positions where the queen can be allocated"""
+    if row == n:
+        print_board(board, n)
+
+    else:
+        for j in range(n):
+            allowed = True
+            for i in range(row):
+                if is_position_safe(board, i, j, row):
+                    allowed = False
+            if allowed:
+                board[row] = j
+                safe_positions(board, row + 1, n)
+
+
+def create_board(size):
+    """Generates the board"""
+    return [0 * size for i in range(size)]
+
+
 if len(sys.argv) != 2:
-    print('Usage: nqueens N')
+    print("Usage: nqueens N")
     exit(1)
 
 try:
-    n_q = int(sys.argv[1])
-except ValueError:
-    print('N must be a number')
+    n = int(sys.argv[1])
+except BaseException:
+    print("N must be a number")
     exit(1)
 
-if n_q < 4:
-    print('N must be at least 4')
+if (n < 4):
+    print("N must be at least 4")
     exit(1)
 
-
-def solve_nqueens(n):
-    '''self descriptive'''
-    if n == 0:
-        return [[]]
-    inner_solution = solve_nqueens(n - 1)
-    return [solution + [(n, i + 1)]
-            for i in range(n_q)
-            for solution in inner_solution
-            if safe_queen((n, i + 1), solution)]
-
-
-def attack_queen(square, queen):
-    '''self descriptive'''
-    (row1, col1) = square
-    (row2, col2) = queen
-    return (row1 == row2) or (col1 == col2) or\
-        abs(row1 - row2) == abs(col1 - col2)
-
-
-def safe_queen(sqr, queens):
-    '''self descriptive'''
-    for queen in queens:
-        if attack_queen(sqr, queen):
-            return False
-    return True
-
-
-for answer in reversed(solve_nqueens(n_q)):
-    result = []
-    for p in [list(p) for p in answer]:
-        result.append([i - 1 for i in p])
-    print(result)
+board = create_board(int(n))
+row = 0
+safe_positions(board, row, int(n))
